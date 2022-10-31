60dcb9d858ff84f745be1d198fa6ce3709663bb2
dmunoz <dmunoz@unl.edu.ec>
2022-10-30 21:43:25 -0500

    Agrega modelo casos de prueba

--- app/proyecto/domain/models.py
+++ app/proyecto/domain/models.py
@@ -4,6 +4,7 @@ from app.core.domain.models import AuditModel
 from app.seguridad.security.middleware import get_username
 from datetime import datetime
 
+
 # Create your models here.
 
 
@@ -18,3 +19,57 @@ class Proyecto(AuditModel):
 
     def __str__(self):
         return '%s' % (self.nombre)
+
+
+class CasoPrueba(AuditModel):
+    TIPO_FUNCIONAL = 'F'
+    TIPO_NO_FUNCIONAL = 'N'
+    TIPO_ESTRUCTURAL = 'E'
+    TIPO_REGRESION = 'R'
+
+    VARIEDAD_POSITVA = '+'
+    VARIEDAD_NEGATIVA = '-'
+
+    PIORIDAD_ALTA = 'A'
+    PIORIDAD_MEDIA = 'M'
+    PIORIDAD_BAJA = 'B'
+
+    EVALUACION_MANUAL = 'M'
+    EVALUACION_AUTOMATICA = 'A'
+
+    ESTADO_BORRADOR = 'B'
+    ESTADO_APROBADA = 'A'
+    ESTADO_BLOQUEADA = 'L'
+    ESTADO_FALLO = 'F'
+
+    CHOICE_TIPO = (
+    (TIPO_FUNCIONAL, 'Funcional'), (TIPO_NO_FUNCIONAL, 'No Funcional'), (TIPO_ESTRUCTURAL, 'Estructural'),
+    (TIPO_REGRESION, 'Regresión'))
+    CHOICE_VARIEDAD = ((VARIEDAD_POSITVA, 'Positiva'), (VARIEDAD_NEGATIVA, 'Negativa'))
+    CHOICE_PRIORIDAD = ((PIORIDAD_ALTA, 'Alta'), (PIORIDAD_MEDIA, 'Media'), (PIORIDAD_BAJA, 'Baja'))
+    CHOICE_EVALUACION = ((EVALUACION_MANUAL, 'Manual'), (EVALUACION_AUTOMATICA, 'Automática'))
+    CHOICE_ESTADO = ((ESTADO_BORRADOR, 'Borrador'), (ESTADO_BLOQUEADA, 'Bloqueda'), (ESTADO_APROBADA, 'Aprobada'),
+                     (ESTADO_FALLO, 'Fallo'))
+
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=80)
+    descripcion = models.TextField(max_length=250, null=True, blank=True)
+    precondicion = models.TextField(max_length=250, null=True, blank=True)
+    resultado_esperado = models.TextField(max_length=250, null=True, blank=True)
+    postcondicion = models.TextField(max_length=250, null=True, blank=True)
+    observacion = models.TextField(max_length=250, null=True, blank=True)
+
+    tipo = models.CharField(max_length=1, choices=CHOICE_TIPO, default=TIPO_FUNCIONAL)
+    variedad = models.CharField(max_length=1, choices=CHOICE_VARIEDAD, default=VARIEDAD_POSITVA)
+    prioridad = models.CharField(max_length=1, choices=CHOICE_PRIORIDAD, default=PIORIDAD_BAJA)
+    evaluacion = models.CharField(max_length=1, choices=CHOICE_EVALUACION, default=EVALUACION_MANUAL)
+    estado = models.CharField(max_length=1, choices=CHOICE_ESTADO, default=ESTADO_BORRADOR)
+
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE, related_name='casos_prueba')
+
+    class Meta:
+        ordering = ('codigo',)
+        unique_together = ('codigo',)
+
+    def __str__(self):
+        return '%s' % (self.codigo)
